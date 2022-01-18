f3a986e54092cd8086702b0971c3127f7b1abfb7
rhksvlf0704 <rhksvlf0704@gmail.com>
2022-01-18 16:48:20 +0900

    Fix: UserController 수정

--- backend-java/src/main/java/com/ssafy/api/request/UserRegisterPostReq.java
+++ backend-java/src/main/java/com/ssafy/api/request/UserRegisterPostReq.java
@@ -5,6 +5,8 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 /**
  * 유저 회원가입 API ([POST] /api/v1/users) 요청에 필요한 리퀘스트 바디 정의.
  */
@@ -12,13 +14,13 @@ import lombok.Setter;
 @Setter
 @ApiModel("UserRegisterPostRequest")
 public class UserRegisterPostReq {
-	@ApiModelProperty(name="유저 ID", example="ssafy205")
+	@ApiModelProperty(name="유저 ID", example="ssafy205") @NotEmpty
 	String userId;
-	@ApiModelProperty(name="유저 Password", example="ssafy")
+	@ApiModelProperty(name="유저 Password", example="ssafy") @NotEmpty
 	String userPw;
-	@ApiModelProperty(name="유저 NickName", example="구미_4반_김은준")
+	@ApiModelProperty(name="유저 NickName", example="구미_4반_김은준") @NotEmpty
 	String userNickname;
-	@ApiModelProperty(name="유저 Phone", example="010-1234-1234")
+	@ApiModelProperty(name="유저 Phone", example="010-1234-1234") @NotEmpty
 	String userPhone;
 	@ApiModelProperty(name="유저 Type", example="1")
 	int userType;
