51e72e5e621a9416dc068da635fe11f57a05b60c
jio-kim <now20412041@gmail.com>
2022-01-19 16:10:55 +0900

    feature/userDistroy

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState, MouseEvent } from "react";
+import axios from "axios";
 import Input from "./common/Input";
 import Button from "./common/Button";
 import style from "../styles/RegisterForm.module.scss";
