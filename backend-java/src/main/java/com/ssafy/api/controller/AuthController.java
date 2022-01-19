399f4496f07159a352c8deb37b4729d250bcb8fe
이진곤 <qazqww@naver.com>
2022-01-19 14:10:22 +0900

    Fix: 로그인 API

--- backend-java/src/main/java/com/ssafy/api/controller/AuthController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.NoSuchElementException;
@@ -22,37 +21,36 @@ import java.util.NoSuchElementException;
  */
 @Api(value = "인증 API", tags = {"Auth."})
 @RestController
-@RequestMapping("/api/v1/auth")
 public class AuthController {
-	@Autowired
-	UserService userService;
-	
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	
-	@PostMapping("/login")
-	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.") 
-    @ApiResponses({
-        @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-        @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
-        @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
-    })
-	public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginPostReq loginInfo) {
-		String userId = loginInfo.getId();
-		String password = loginInfo.getPassword();
-		try {
-			User user = userService.getUserByUserId(userId);
-			// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
-			if(passwordEncoder.matches(password, user.getUserPw())) {
-				// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-				return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userId)));
-			}
-			// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-			return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
-		}catch (NoSuchElementException e){
-			return ResponseEntity.status(404).body(UserLoginPostRes.of(404, "사용자 없음", null));
-		}
+    @Autowired
+    UserService userService;
 
-	}
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    @ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value = "로그인 요청 정보", required = true) UserLoginPostReq loginInfo) {
+        String userId = loginInfo.getId();
+        String password = loginInfo.getPassword();
+        try {
+            User user = userService.getUserByUserId(userId);
+            // 패스워드 일치 여부 확인
+            if (passwordEncoder.matches(password, user.getUserPw())) {
+                // 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
+                return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userId)));
+            }
+            // 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
+            return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404).body(UserLoginPostRes.of(404, "사용자 없음", null));
+        }
+
+    }
 }
