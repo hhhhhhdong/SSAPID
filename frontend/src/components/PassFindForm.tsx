af1d033796d55eaa48cb4655a58b88e79fe1cf63
jio-kim <now20412041@gmail.com>
2022-01-21 11:22:23 +0900

    Fix: idFindForm

--- frontend/src/components/PassFindForm.tsx
+++ frontend/src/components/PassFindForm.tsx
@@ -1,68 +1,78 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import Button from "./common/Button";
 import Input from "./common/Input";
 import style from "../styles/globalForm.module.scss";
 
 type PasswordFindProps = {
-  onSubmit: (form: { id: string }) => void;
+  onSubmit: (form: { userId: string }) => void;
 };
 
 function PassFindForm({ onSubmit }: PasswordFindProps) {
   const submitButtonType = "submit";
-  const backButtonType = "button";
   const idPlaceHolder = "이메일을 입력하세요.";
-  const url = ["/"];
-  const navigate = useNavigate();
-  const [isLoad, setLoad] = useState(false);
+
   const [isEmpty, setEmpty] = useState(true);
-  const backOut = () => {
-    // 뒤로가기 로직
-    alert("backOut");
-    setLoad(true);
-    navigate(url[0]);
-  };
+
   const [form, setForm] = useState({
-    id: "",
+    userId: "",
   });
-  const { id } = form;
+  const { userId } = form;
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
-    });
+    const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+    if (korean.test(value)) {
+      alert("한글은 입력하실 수 없습니다.");
+      setForm({
+        ...form,
+      });
+    } else {
+      setForm({
+        ...form,
+        [name]: value,
+      });
+    }
   };
 
   useEffect(() => {
-    if (id === "") {
+    if (userId === "") {
       setEmpty(true);
     } else {
       setEmpty(false);
     }
   });
+  const submit = () => {
+    axios.post("/user/find-pw", form).then((res) => {
+      console.log(res.status);
+      if (true) {
+        // 인증번호를 보내드렸습니다. 이메일로
+        // 인증번호를 입력하세요.
+        // 확인되면 비밀번호 변경 페이지
+        // 인증번호 입력
+      }
+    });
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onSubmit(form);
     setForm({
-      id: "",
+      userId: "",
     });
   };
   return (
     <form className={style.form} onSubmit={handleSubmit}>
       <Input
-        name="id"
-        value={id}
+        name="userId"
+        value={userId}
         onChange={onChange}
         placeHolder={idPlaceHolder}
       />
-      <Button buttonType={submitButtonType} Disabled={isEmpty} text="다음" />
       <Button
-        buttonType={backButtonType}
-        text="뒤로가기"
-        handleClick={backOut}
-        Disabled={isLoad}
+        buttonType={submitButtonType}
+        handleClick={submit}
+        Disabled={isEmpty}
+        text="비밀번호 찾기"
       />
     </form>
   );
