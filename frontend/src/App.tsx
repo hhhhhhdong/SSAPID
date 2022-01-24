868a8bfd03426cb884f38c57ca4e56c11909cda7
hhhhhhdong <gusehd520@gmail.com>
2022-01-24 16:15:18 +0900

    Add: Main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Main from "./pages/Main";
-import IdFind from "./pages/IdFind";
-import Register from "./pages/Register";
-import PassFind from "./pages/PassFind";
-import Login from "./pages/Login";
-import UserDist from "./pages/UserDist";
+import IdFind from "pages/IdFind";
+import Register from "pages/Register";
+import PassFind from "pages/PassFind";
+import Login from "pages/Login";
+import UserDist from "pages/UserDist";
+import Main from "pages/Main";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* <Route path="/" element={<Main />} /> */}
+          <Route path="/" element={<Main />} />
           <Route path="/register" element={<Register />} />
           <Route path="/idFind" element={<IdFind />} />
           <Route path="/passFind" element={<PassFind />} />
