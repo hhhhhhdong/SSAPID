578a1ce2ffddab352ebacb58b5a4d2af311986db
hhhhhhdong <gusehd520@gmail.com>
2022-01-27 14:28:13 +0900

    Add: sidebar 애니메이션

--- frontend/src/components/NavbarForm.tsx
+++ frontend/src/components/NavbarForm.tsx
@@ -1,16 +1,58 @@
-import React from "react";
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { openSidebar } from "redux/_actions/actions";
 import style from "styles/NavbarForm.module.scss";
+import { RootState } from "redux/_reducers";
 
 function NavbarForm() {
+  const dispatch = useDispatch();
+  const open = useSelector((state: RootState) => state.userReducer.openSidebar);
+  const [openIcon, setOpenIcon] = useState<boolean>(false);
+  const [closeIcon, setCloseIcon] = useState<boolean>(false);
+
+  const onSidebarMouseEnter = () => {
+    if (!open) {
+      setOpenIcon(true);
+    } else {
+      setCloseIcon(true);
+    }
+  };
+  const onSidebarMouseLeave = () => {
+    setOpenIcon(false);
+    setCloseIcon(false);
+  };
+
+  const onClickSide = () => {
+    dispatch(openSidebar(!open));
+  };
+
+  const displayMenuIcon = () => {
+    if (open) {
+      return <i className="fas fa-angle-right" style={{ fontSize: "18px" }} />;
+    }
+    if (openIcon) {
+      return <i className="fas fa-angle-left" style={{ fontSize: "18px" }} />;
+    }
+    return <i className="fas fa-align-justify" />;
+  };
+
   return (
-    <nav className={style.nav}>
+    <nav className={open ? `${style.nav} ${style.navMove}` : style.nav}>
       <h2>SSAFID</h2>
       <div>
         <span>글쓰기</span>
         <span>내정보</span>
         <span>로그아웃</span>
-        <span>
-          <i className="fas fa-align-justify" />
+        <span
+          onMouseEnter={onSidebarMouseEnter}
+          onMouseLeave={onSidebarMouseLeave}
+          onClick={onClickSide}
+          role="button"
+          className={style.navSidebarIcon}
+        >
+          {displayMenuIcon()}
         </span>
       </div>
     </nav>
