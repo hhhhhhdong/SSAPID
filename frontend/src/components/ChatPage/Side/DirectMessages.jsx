a07caf18e7dd776cc4bd79a3ff5693cd86fc3927
jio-kim <now20412041@gmail.com>
2022-02-02 21:52:51 +0900

    Add: chatServ

--- frontend/src/components/ChatPage/Side/DirectMessages.jsx
+++ frontend/src/components/ChatPage/Side/DirectMessages.jsx
@@ -1,27 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { BiMessageAltDetail } from "react-icons/bi";
+import { useDispatch } from "react-redux";
 import {
   getDatabase,
   ref,
   DataSnapshot,
   onChildAdded,
 } from "firebase/database";
+import { chatRoomString } from "redux/_actions/actions";
 
 function DirectMessages() {
+  // DB에서 users 스키마를 res 해서 state
   const [state, setState] = useState({
     usersRef: ref(getDatabase(), "users"),
   });
+  // 가져온 users 스키마를 바탕으로 users state 형성
   const [users, setUsers] = useState([]);
+  const dispatch = useDispatch();
 
+  // 렌더링 될때마다 db에서 스키마 로딩
   useEffect(() => {
     addUsersListeners();
   }, []);
 
-  // 데이터 스냅샷을 이용해서 DB에서 스키마를 들고와서 로그인 로그아웃 기능을 추가함
+  // 데이터 스냅샷을 이용해서 DB에서 스키마를 가지고 조작
   function addUsersListeners() {
     const { usersRef } = state;
     const myName = sessionStorage.getItem("userNickname");
     const usersArray = [];
+
     onChildAdded(usersRef, (DataSnapshot) => {
       if (myName !== DataSnapshot.key) {
         // eslint-disable-next-line prefer-const
@@ -45,8 +52,12 @@ function DirectMessages() {
 
   // 채팅 룸 변경
   const changeChatRoom = (user) => {
-    const chatRoomId = getChatRoomId(user.userNickname);
-    return chatRoomId;
+    const chatRoomId = getChatRoomId(user.nickName);
+    const chatRoomData = {
+      id: chatRoomId,
+      name: user.nickName,
+    };
+    dispatch(chatRoomString(chatRoomData));
   };
 
   // 닉네임 렌더링
@@ -67,7 +78,7 @@ function DirectMessages() {
       <span style={{ display: "flex", alignItems: "center" }}>
         <BiMessageAltDetail style={{ marginRight: "3px" }} /> Direct Message(1)
       </span>
-      <ul style={{ listStyleType: "none", padding: 0 }}>
+      <ul style={{ listStyleType: "none", padding: 0, cursor: "pointer" }}>
         {renderDirectMessages(users)}
       </ul>
     </div>
