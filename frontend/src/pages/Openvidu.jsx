fa1d6e53bfc069b73095e7e53d84a1d8574f4a0b
이진곤 <qazqww@naver.com>
2022-02-07 16:19:22 +0900

    Add: WebRTC Session, Token 생성 백엔드에서 처리하도록

--- frontend/src/pages/Openvidu.jsx
+++ frontend/src/pages/Openvidu.jsx
@@ -10,8 +10,6 @@ import OpenViduSession from "openvidu-react";
 class Openvidu extends Component {
   constructor(props) {
     super(props);
-    this.OPENVIDU_SERVER_URL = `https://${window.location.hostname}:4443`;
-    this.OPENVIDU_SERVER_SECRET = "MY_SECRET";
     this.state = {
       mySessionId: "SessionA",
       myUserName: `OpenVidu_User_${Math.floor(Math.random() * 100)}`,
@@ -63,6 +61,15 @@ class Openvidu extends Component {
       });
       event.preventDefault();
     }
+  }
+
+  getToken() {
+    // axios.post(`/session`, data).then(
+    // ).catch();
+
+    return this.createSession(this.state.mySessionId)
+      .then((sessionId) => this.createToken(sessionId))
+      .catch((Err) => console.error(Err));
   }
 
   render() {
@@ -117,88 +124,6 @@ class Openvidu extends Component {
         )}
       </div>
     );
-  }
-
-  /**
-   * --------------------------
-   * SERVER-SIDE RESPONSIBILITY
-   * --------------------------
-   * These methods retrieve the mandatory user token from OpenVidu Server.
-   * This behaviour MUST BE IN YOUR SERVER-SIDE IN PRODUCTION (by using
-   * the API REST, openvidu-java-client or openvidu-node-client):
-   *   1) Initialize a Session in OpenVidu Server	(POST /openvidu/api/sessions)
-   *   2) Create a Connection in OpenVidu Server (POST /openvidu/api/sessions/<SESSION_ID>/connection)
-   *   3) The Connection.token must be consumed in Session.connect() method
-   */
-
-  getToken() {
-    return this.createSession(this.state.mySessionId)
-      .then((sessionId) => this.createToken(sessionId))
-      .catch((Err) => console.error(Err));
-  }
-
-  createSession(sessionId) {
-    return new Promise((resolve, reject) => {
-      const data = JSON.stringify({ customSessionId: sessionId });
-      axios
-        .post(`${this.OPENVIDU_SERVER_URL}/openvidu/api/sessions`, data, {
-          headers: {
-            Authorization: `Basic ${btoa(
-              `OPENVIDUAPP:${this.OPENVIDU_SERVER_SECRET}`
-            )}`,
-            "Content-Type": "application/json",
-          },
-        })
-        .then((response) => {
-          console.log("CREATE SESION", response);
-          resolve(response.data.id);
-        })
-        .catch((response) => {
-          const error = { ...response };
-          if (error.response && error.response.status === 409) {
-            resolve(sessionId);
-          } else {
-            console.log(error);
-            console.warn(
-              `No connection to OpenVidu Server. This may be a certificate error at ${this.OPENVIDU_SERVER_URL}`
-            );
-            if (
-              window.confirm(
-                `No connection to OpenVidu Server. This may be a certificate error at "${this.OPENVIDU_SERVER_URL}"\n\nClick OK to navigate and accept it. ` +
-                  `If no certificate warning is shown, then check that your OpenVidu Server is up and running at "${this.OPENVIDU_SERVER_URL}"`
-              )
-            ) {
-              window.location.assign(
-                `${this.OPENVIDU_SERVER_URL}/accept-certificate`
-              );
-            }
-          }
-        });
-    });
-  }
-
-  createToken(sessionId) {
-    return new Promise((resolve, reject) => {
-      const data = JSON.stringify({});
-      axios
-        .post(
-          `${this.OPENVIDU_SERVER_URL}/openvidu/api/sessions/${sessionId}/connection`,
-          data,
-          {
-            headers: {
-              Authorization: `Basic ${btoa(
-                `OPENVIDUAPP:${this.OPENVIDU_SERVER_SECRET}`
-              )}`,
-              "Content-Type": "application/json",
-            },
-          }
-        )
-        .then((response) => {
-          console.log("TOKEN", response);
-          resolve(response.data.token);
-        })
-        .catch((error) => reject(error));
-    });
   }
 }
 
