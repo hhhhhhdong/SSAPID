cf3ac314d623ccd4d018533cad5350b9a2d6839c
이진곤 <qazqww@naver.com>
2022-02-07 16:20:59 +0900

    Add: WebRTC Session, Token 생성 백엔드에서 처리하도록

--- backend-java/src/main/java/com/ssafy/api/request/SessionCreateReq.java
+++ backend-java/src/main/java/com/ssafy/api/request/SessionCreateReq.java
@@ -1,2 +1,22 @@
-package com.ssafy.api.request;public class CreateSessionReq {
+package com.ssafy.api.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@ApiModel("CreateSessionRequest")
+public class SessionCreateReq {
+
+    @ApiModelProperty(name = "세션 이름", example = "SessionA")
+    @NotEmpty
+    String sessionName;
+    @ApiModelProperty(name = "사용자 닉네임", example = "김싸피")
+    @NotEmpty
+    String userNickname;
+
 }
